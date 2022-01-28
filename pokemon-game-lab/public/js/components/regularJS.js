0bcdabac843d8885b9d4d958b544c4d2c097b99a
Ray Anthoney <rmaxey911@gmail.com>
2022-01-28 10:16:37 -0700

    added psuedo code to .js and created attack states

--- pokemon-game-lab/public/js/components/regularJS.js
+++ pokemon-game-lab/public/js/components/regularJS.js
@@ -5,6 +5,8 @@ webpackJsonp([0],[
 "use strict";
 
 
+//  This is the Database
+
 var pokemonDB = [{
   name: 'charmander',
   type: 'fire',
@@ -31,51 +33,99 @@ var pokemonDB = [{
   img: 'http://www.smogon.com/dex/media/sprites/xy/squirtle.gif'
 }];
 
+//  State
 var gameState = {
   userPokemon: '',
   rivalPokemon: ''
 };
 
 console.log(gameState);
+
+//  Elements
 var pokemonsEl = document.querySelector('.select-screen').querySelectorAll('.character');
 console.log(pokemonsEl);
 var battleScreenEl = document.getElementById('battle-screen');
-var i = 0;
+var attackBtnsEl = document.getElementById('battle-screen').querySelectorAll('.attack');
+console.log(attackBtnsEl);
 
+//  This is the Initial Loop
+var i = 0;
 while (i < pokemonsEl.length) {
+  //  This adds function to all the characters on screen select
   pokemonsEl[i].onclick = function () {
+    //  Current Selected Pokemons Name
     var pokemonName = this.dataset.pokemon;
+
+    //  Elements for Images on the Battle Screen
     var player1img = document.querySelector('.player1').getElementsByTagName('img');
     var player2img = document.querySelector('.player2').getElementsByTagName('img');
 
+    //  This is Where we Save the Current Pokemon
     gameState.userPokemon = pokemonName;
 
+    //  CPU Picks a Pokemon
     cpuPick();
+    //  Changes the Select Screen to Battle Screen
     battleScreenEl.classList.toggle('active');
 
+    //  Select Data From Current User Pokemon
     var currentPokemon = pokemonDB.filter(function (pokemon) {
       return pokemon.name == gameState.userPokemon;
     });
+    player1img[0].src = currentPokemon[0].img;
 
+    //  Select Data From Current CPU Pokemon   
     var currentRivalPokemon = pokemonDB.filter(function (pokemon) {
       return pokemon.name == gameState.rivalPokemon;
     });
-
-    player1img[0].src = currentPokemon[0].img;
     player2img[0].src = currentRivalPokemon[0].img;
 
-    console.log(currentPokemon);
+    //  User Choose Attack
+
+
+    //  CPU Health Goes Down
+
+
+    //  CPU Attack
+
+
+    //  User Health Goes Down
+
+
+    //  Rock > Scissors
+
+
+    //  Paper > Rock
+
+
+    //  Scissors > Paper
+
+
+    //  Depending on Pokemon Type and Defense is How Hard the Attack is Going to be
+    //  and How Much Health it Will Take Out.
+
+
+    //  Then Whoever Gets to Health < = 0 Loses
   };
   i++;
 }
+var a = 0;
+while (a < attackBtnsEl.length) {
+  attackBtnsEl[a].onclick = function () {
+    var attackName = this.dataset.attack;
+    gameState.currentUserAttack = attackName;
+    console.log(gameState.currentUserAttack);
+  };
+  a++;
+}
 
-function randomNumber(min, max) {
+var randomNumber = function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-function cpuPick() {
+var cpuPick = function cpuPick() {
   gameState.rivalPokemon = pokemonsEl[randomNumber(0, 3)].dataset.pokemon;
-}
+};
 
 // // pokemon
 // // create data for 3 different pokemons, with their names, type, weaknesses, health, and attack moves(name, attack stat, maximum)
