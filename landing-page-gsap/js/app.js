4baeeaa8918029323b139c48380006acd2c5efdf
Ray Anthoney <rmaxey911@gmail.com>
2022-02-07 07:29:11 -0700

    added app.js and started to animate elements

--- landing-page-gsap/js/app.js
+++ landing-page-gsap/js/app.js
@@ -1,1 +1,32 @@
-console.log('Whats Happening')
+let tl = gsap.timeline()
+tl.fromTo('.logo', {
+    x: -200,
+    opacity: 0,
+},
+{
+    x: 0,
+    opacity: 1,
+    delay: .5,
+    duration: 1.3,
+}
+).fromTo('.menu', {
+    x: 200,
+    opacity: 0,
+},
+{
+    x: 0,
+    opacity: 1,
+    delay: .5,
+    duration: 1.3,
+},
+    ">-1.8"
+)
+.fromTo('.girl', {
+    height: 0,
+},
+{
+    height: 600,
+    duration: 1.4,
+    ease: Power2.inOut
+}
+);
