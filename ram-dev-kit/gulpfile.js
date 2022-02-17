71f0762031b3a91281a4f9c004453c4c32e0dc73
Ray Anthoney <rmaxey911@gmail.com>
2022-02-17 16:42:28 -0700

    Codingphase development kit added gulp, sass

--- ram-dev-kit/gulpfile.js
+++ ram-dev-kit/gulpfile.js
@@ -1,11 +1,51 @@
-var gulp = require('gulp');
 
-gulp.task('printName', function(){
-    console.log('My name is RayAnthoney')
+const gulp = require('gulp');
+
+const sass = require('gulp-sass')(require('sass'));
+
+const autoprefixer = require('gulp-autoprefixer');
+
+const browserSync = require('browser-sync').create();
+
+const cleanCSS = require('gulp-clean-css');
+
+const reload = browserSync.reload
+
+
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream())
+});
+
+// Opens New Browser in Default
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: './public',
+        notify: false,
+        open: false 
+    })
 })
 
-gulp.task('printAge', function(){
-    console.log('My age is infinite')
+gulp.task('default', ['sass', 'browser-sync'], function(){
+    gulp.watch('./src/scss/**/*', ['sass'])
 })
 
-gulp.task('default', ['printName', 'printAge'])
+gulp.task('sass;minify', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./public/css'))
+});
+
+
+// gulp.task('production', ['sass:minify'])
+
+
