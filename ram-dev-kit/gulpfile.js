6399b710a4ed46d0ae49d3dd86f8af30a43c2b06
Ray Anthoney <rmaxey911@gmail.com>
2022-02-17 20:25:10 -0700

    Codingphase adding css minify and sourcemaps

--- ram-dev-kit/gulpfile.js
+++ ram-dev-kit/gulpfile.js
@@ -1,51 +1,50 @@
+const gulp = require("gulp");
 
-const gulp = require('gulp');
+const sass = require("gulp-sass")(require("sass"));
 
-const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require("gulp-autoprefixer");
 
-const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require("browser-sync").create();
 
-const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
-const cleanCSS = require('gulp-clean-css');
+const cleanCSS = require("gulp-clean-css");
 
-const reload = browserSync.reload
+const sourcemaps = require('gulp-sourcemaps');
 
-
-gulp.task('sass', function () {
-  return gulp.src('./src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        cascade: false
-    }))
-    .pipe(gulp.dest('./public/css'))
-    .pipe(browserSync.stream())
+gulp.task("sass", function () {
+  return gulp
+    .src("./src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest("./public/css"))
+    .pipe(browserSync.stream());
 });
 
 // Opens New Browser in Default
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: './public',
-        notify: false,
-        open: false 
-    })
-})
-
-gulp.task('default', ['sass', 'browser-sync'], function(){
-    gulp.watch('./src/scss/**/*', ['sass'])
-})
-
-gulp.task('sass;minify', function () {
-  return gulp.src('./src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        cascade: false
-    }))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./public/css'))
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: "./public",
+    notify: false,
+    open: false,
+  });
 });
 
+gulp.task("sass:minify", function () {
+  return gulp
+    .src("./public/css/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./public/css"));
+});
 
-// gulp.task('production', ['sass:minify'])
+gulp.task("default", ["sass", "browser-sync"], function () {
+  gulp.watch("./src/scss/**/*", ["sass"]);
+});
 
-
+gulp.task("production", ["sass:minify"])
