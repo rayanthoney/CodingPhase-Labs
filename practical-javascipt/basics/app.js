60c9d4f7ecec568eb41721f1d14d6b834bff0842
Ray Anthoney <rmaxey911@gmail.com>
2022-02-23 18:22:55 -0700

    CodingPhase practical javascipt practice of concepts

---
+++ practical-javascipt/basics/app.js
@@ -0,0 +1,40 @@
+// console.log('Hi we just loaded the app.js')
+// console.log(2+2);
+// console.log('Ray ' + 'Anthoney')
+
+
+
+// function printName(first, last) {
+//     console.log(first + ' ' + last);
+//     alert(first + ' ' + last);
+// }
+
+
+// "Everything in programming is an an action that triggers a reaction"
+
+// Variables - is a bucket that you give data 
+    let username = "Ray Anthoney";
+    console.log(username)
+    username = "Mireya Elisse";
+    console.log(username);
+
+// Constants
+    const store = "Diamonds";
+    // username = "New Balance";
+    console.log(username);
+
+// Operators - symbol that tells the program an action 
+
+    // let x = 25;
+    // x = 25 + 5;
+    // console.log(x)
+   
+    // let x = 25;
+    // x = x + 5;
+    // console.log(x)
+   
+    let x = 25;
+    x += 5;
+    console.log(x)
+
+// Strings - any text you put in quotes
