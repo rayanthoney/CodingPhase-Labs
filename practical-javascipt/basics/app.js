e3043bce60b0aeede2282ff5b0ce87b6e5f83899
Ray Anthoney <rmaxey911@gmail.com>
2022-02-23 19:18:30 -0700

    CodingPhase practical javascript concepts practice

--- practical-javascipt/basics/app.js
+++ practical-javascipt/basics/app.js
@@ -38,3 +38,29 @@
     console.log(x)
 
 // Strings - any text you put in quotes
+    console.log("username: " + "Sir Kairos")
+
+// Numbers - numbers
+    console.log((((2*4)-3) + ((25%3) + 20) ) / 4);
+    let y = 10;
+    console.log(y)
+
+    let age = 18;
+    console.log(age == 21 && username == "Mireya Elisse");
+
+// Array - list of values 
+    let listAR = [23, 234, "billy", "joe", {title: 'harry potter'}];
+
+    console.log(listAR)
+
+// Objects - A entity that holds values and methods
+
+    let user = {
+        name: "Kairos",
+        username: "MireyaElisse",
+        favoriteMovies: ['SW', 'Lord Of The Flies'],
+        age: 10,
+        princeCity: function () {
+            return 'New York City'
+        }
+    };
