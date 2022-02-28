6cecf629e7e6f3a2e58846b8b2d559c430fc08b7
Ray Anthoney <rmaxey911@gmail.com>
2022-02-27 18:37:12 -0700

    100Devs .js added EventListener and close modal eventListener

--- practical-javascipt/scenario1/js/app.js
+++ practical-javascipt/scenario1/js/app.js
@@ -3,7 +3,14 @@
 
 
 window.onload = function(){
-    let emailModal = document.getElementsByClassName('email-modal')[0] 
+    let emailModal = document.getElementsByClassName('email-modal')[0];
+
+    let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+
+    closeModal.addEventListener('click', e => {
+        emailModal.classList.remove('email-modal--visible');
+    });
+    console.log(closeModal);
 
     
     console.log(emailModal)
