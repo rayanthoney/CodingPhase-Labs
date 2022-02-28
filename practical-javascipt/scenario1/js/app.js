c215cdab9eb9ceeb27a7da8f8e5107d6f5c582ea
Ray Anthoney <rmaxey911@gmail.com>
2022-02-28 07:30:34 -0700

    Codingphase practical Javascipt added trigger functions cleaned code

--- practical-javascipt/scenario1/js/app.js
+++ practical-javascipt/scenario1/js/app.js
@@ -1,23 +1,27 @@
 
-
-
-
 window.onload = function(){
+// Variables
+    let emailState = false;
+
     let emailModal = document.getElementsByClassName('email-modal')[0];
 
     let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+// Variable = let Function = if
+    let showModal = () => {        
+       if(emailState == false) {
+           emailModal.classList.add('email-modal--visible');
+           emailState = true 
+        }
+    }
 
-    closeModal.addEventListener('click', e => {
+    closeModal.addEventListener('click', () => {
         emailModal.classList.remove('email-modal--visible');
     });
-    console.log(closeModal);
-
-    
-    console.log(emailModal)
-    document.documentElement.addEventListener('mouseleave', e => {
-        emailModal.classList.add('email-modal--visible');
-       console.log('mouse left')
+// Trigger Function   
+    document.body.addEventListener('mouseleave', () => {  
+        showModal();
     });
+    
 
 
     console.log(document)
