d572694be7dd7cd032c23a5f865ecfa8bba150da
Ray Anthoney <rmaxey911@gmail.com>
2022-02-28 19:56:20 -0700

    Codingphase practical Js finished leave on click modal

--- practical-javascipt/scenario1/js/app.js
+++ practical-javascipt/scenario1/js/app.js
@@ -6,6 +6,26 @@ window.onload = function(){
     let emailModal = document.getElementsByClassName('email-modal')[0];
 
     let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+    
+    let emailInput = document.getElementsByClassName('email-modal__input')[0];
+    
+    let emailButton = document.getElementsByClassName('email-modal__button')[0];
+    
+// Function to validate email (RegExp)
+    function emailIsValid (email) {
+        return /\S+@\S+\.\S+/.test(email)
+    }
+
+// Event Listener for the click button on form
+    emailButton.addEventListener('click', () => {
+        if(emailIsValid(emailInput.value)) {
+            console.log(emailInput.value)  
+         } else {
+        document.getElementsByClassName('email-modal__form-group')[0].classList.add('email-modal__form-group--error')
+        document.getElementsByClassName('email-modal__error-message')[0].classList.add('email-modal__error-message--active')        
+         }         
+    })
+
 // Variable = let Function = if
     let showModal = () => {        
        if(emailState == false) {
