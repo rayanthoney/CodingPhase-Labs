e81d7e3c4f4ddb01f2e1d4040352d85e4958ceca
Ray Anthoney <rmaxey911@gmail.com>
2022-03-05 08:55:49 -0700

    Codingphase added setInterval to js

--- practical-javascipt/scenario2/js/app.js
+++ practical-javascipt/scenario2/js/app.js
@@ -10,7 +10,32 @@ let init = () => {
 	slides[0].classList.add("jumbo-slider__slide--active");
 	links[current - 1].classList.add("jumbo-slider__link--active");
 	// 13:45 I am adding these by myself for now
-	circles[current - 1].classList.add("jumbo-slider__circle--filled");
+	//circles[current - 1].classList.add("jumbo-slider__circle--filled");
+
+	// Update elipses and links
+	let updateNav = () => {
+		console.log(`update current: ${current}`);
+		for (let index = 0; index < slides.length; index++) {
+			links[index].classList.remove("jumbo-slider__link--active");
+			circles[index].classList.remove("jumbo-slider__circle--filled");
+		}
+
+		links[current - 1].classList.add("jumbo-slider__link--active");
+		circles[current - 1].classList.add("jumbo-circle--filled");
+	};
+	let startSliding = () => {
+		setInterval(() => {
+			console.log(`current: ${current}`);
+			slides[1].classList.add("jumbo-slider__slide--active");
+			slides[0].classList.remove("jumbo-slider__slide--active");
+
+			if (current < slides.length) {
+				current++;
+				updateNav(current);
+			}
+		}, 6000);
+	};
+	startSliding();
 };
 
 init();
