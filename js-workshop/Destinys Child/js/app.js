3eb0a02420208e74686ebd7a1dc42627e12a6949
Ray Anthoney <rmaxey911@gmail.com>
2022-03-31 07:45:59 -0600

    Codingphase js workshop Day 2 creating Destiny Child practice

---
+++ js-workshop/Destinys Child/js/app.js
@@ -0,0 +1,20 @@
+let artist = [
+    {
+        name: 'Kelly',
+        image_url: 'https://doodleipsum.com/200/avatar?i=7a4f635d4afc2cca4ba6f94895bb0566',
+        position: '-240px;'
+
+    },
+    {
+        name: 'Beyaonce',
+        image_url: 'https://doodleipsum.com/200/avatar?i=995e72be48e90a0aa13341bf2de29179',
+        position: '0'
+
+    },
+    {
+        name: 'Michelle',
+        image_url: 'https://doodleipsum.com/200/avatar?i=5dc583219eaed67919876becac0d62a2',
+        position: '240'
+
+    }
+]
