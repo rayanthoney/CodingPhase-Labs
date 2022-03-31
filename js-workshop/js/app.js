95a25c49fdcc9215a5efba43ded376b6c2adad3b
Ray Anthoney <rmaxey911@gmail.com>
2022-03-30 21:25:49 -0600

    Codingphase js workshop 2 material

--- js-workshop/js/app.js
+++ js-workshop/js/app.js
@@ -1,42 +1,63 @@
-// Javascript goes here
-console.log('Hello Codingphase World!')
+// JavaScript code goes here
+console.log("Hello Codingphase World :)");
 
-// // Variable
-// let car = '1967 Chevy Camaro'
-//     alert(car)
+// document.getElementById('main').style.backgroundColor = 'red';
+// document.getElementById('main').style.height = '200px';
+// document.getElementById('main').style.width = '200px';
 
-// Method Object
-// let car = {
-//     title: "Camaro",
-//     wheels: 4,
-//     windows: 6,
-//     printMessage: function(){
-//         console.log('This Camaro has 4 wheels and 6 windows')
-//     }
-// }
-// console.log(window.document) // will show object list
-// alert(car) // will show a pop up
+////////////////////////////
+// or use box = to 'main' //
+////////////////////////////
 
-let main = document.getElementById('main')
-let box = document.getElementsByClassName('box')
-let printBox = (index) => {
-    return `This box has a height ${box[index].clientHeight} and a width of ${box[index].clientWidth}`
-}
+// Variable = box = main
+// let box = document.getElementById('main')
+
+// setTimeout(( ) => {
+
+//     box.style.background = 'linear-gradient(45deg, rgba(95,7,221,1) 0%, rgba(255,0,213,1) 100%)';
+//     box.style.height = '400px';
+//     box.style.width = '400px';
+//     box.classList.add('animation-in')
+
+// }, 200)
 
 
 
-// create an h1 element
-let h1 = document.createElement('h1');
-// give it some content
-let titleText = document.createTextNode('Mary had a lil lamb');
-// add text node to the newly created h1
-h1.appendChild(titleText)
-// add the newly created element and its content into the DOM
-document.body.appendChild(h1)
 
+let box = document.getElementById("main");
+let btnNum1 = document.getElementById('btn-num1');
+let btnNum2 = document.getElementById('btn-num2');
+let btnNum3 = document.getElementById('btn-num3');
+let btnExit = document.getElementById('btn-exit');
 
-main.innerHTML =
-`<div>
-    <h2>Lorem Doodle</h2>
-    <img src="https://doodleipsum.com/700/flat?i=1fd425a431f7f9d55bb237a7c39f3f73">
-</div>`
+let initBox = () => {
+	box.style.background = '#5f07dd';
+	box.style.height = "200px";
+	box.style.width = "200px";
+};
+
+initBox();
+
+let animationStart = () => {
+		box.classList.add("animation-in");
+};
+
+let animationReverse = () => {
+	box.classList.remove("animation-in");
+};
+
+btnNum1.addEventListener("click", () => {
+	animationStart();
+});
+
+btnNum2.addEventListener("click", () => {
+	animationStart();
+});
+
+btnNum3.addEventListener("click", () => {
+	animationStart();
+});
+
+btnExit.addEventListener("click", () => {
+	animationReverse();
+});
