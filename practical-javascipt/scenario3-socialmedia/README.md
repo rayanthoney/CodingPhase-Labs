0de54d86a9c92e13db15f62323838d5fe64e0b47
Ray Anthoney <rmaxey911@gmail.com>
2022-05-04 14:39:03 -0600

    Codingphase Practical JavaScript adding image and link

---
+++ practical-javascipt/scenario3-socialmedia/README.md
@@ -0,0 +1,45 @@
+# Codingphase Practical JavaScript project "Social Media Card"
+
+<br>
+<br>
+<br>
+
+<!-- Banner Section -->
+<h2 align="center">🛠️ Projects</h2>
+
+<br>
+<br>
+
+<div align="center" >
+    <a href=''>
+        <img src="img/memo-brown.png" alt="Social Media Card" width="300"/>
+    </a>
+
+<br>
+<br>
+
+<p>
+    <a href="https://github.com/rayanthoney/CodingPhase-Labs/tree/master/practical-javascipt/scenario2" target="_blank">
+            <img src="https://img.shields.io/badge/Repo-lightgrey?style=plastic&zfor-the-badge&logo=github"/>
+    </a>
+    <a href="" target="_blank">
+        <img src="https://img.shields.io/badge/-website-green?style=plastic&zfor-the-badge&color=e8970c"/>
+    </a>
+</p>
+</div>
+
+<br>
+
+## 🎨 This project was built using these technologies.
+
+<br>
+
+<p>
+<strong>JavaScript, CSS3, HTML5</strong> - JavaScript version of a Social Media Card displaying the ability to dynamically access your social media websites form one card.
+</p>
+
+<br>
+
+[![HTML5 Badge](https://img.shields.io/badge/-HTML-E44D26?style=plastic&for-the-badge&labelColor=black&logo=html5&logoColor=E44D26)](#)&nbsp;
+[![CSS3 Badge](https://img.shields.io/badge/-CSS-1572B6?&style=plastic&for-the-badge&labelColor=black&logo=css3&logoColor=1572B6)](#)&nbsp;&nbsp;
+[![Javascript Badge](https://img.shields.io/badge/-Javascript-F0DB4F?style=plastic&for-the-badge&labelColor=black&logo=javascript&logoColor=F0DB4F)](#)&nbsp;&nbsp;
---
+++ practical-javascipt/scenario3-socialmedia/js/app.js
@@ -0,0 +1,225 @@
+// Object:
+let data = {
+	fullName: "Memo Brown",
+	position: "UI/UX Designer",
+	socials: [
+		{
+			id: "fb",
+			service: "Facebook",
+			url: "https://www.facebook.com/mbrown0109",
+			icon: "fab fa-facebook-f",
+		},
+		{
+			id: "ig",
+			service: "Instagram",
+			url: "https://www.instagram.com/mbrown0109",
+			icon: "fab fa-instagram",
+		},
+		{
+			id: "db",
+			service: "Dribbble",
+			url: "https://www.dribbble.com/mbrown0109",
+			icon: "fab fa-dribble",
+		},
+		{
+			id: "gl",
+			service: "Google",
+			url: "https://www.google.com/mbrown0109",
+			icon: "fab fa-facebook",
+		},
+	],
+};
+let cardApp = (data) => {
+	let body = document.getElementsByTagName("body")[0];
+	let root = document.createElement("div");
+	let style = document.createElement("style");
+	let card = document.createElement("section");
+	let html = `
+    <div class="card__wrapper">
+        <img src="img/memoBrown.png" alt="User Image" class="card__user-img ">
+        <div class="card__info">
+            <span class="card__name">${data.fullName}</span>
+            <span class="card__title">${data.position}</span>
+        </div>
+
+        <div class="card__socials">
+            
+        </div>
+    </div>
+`;
+	let cssStyles = `
+    .wrapper {
+        background: #197278;
+        min-height: 100vh;
+    }
+
+    .wrapper::before {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(#c44536, #772e25);
+        clip-path: circle(30% at left 70%);
+    }
+    .wrapper::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(#edddd4, #c44536);
+        clip-path: circle(20% at right 10%);
+    }
+
+    .card {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .card__wrapper {
+        // height: 400px;
+        padding: 20px 20px 3rem 20px;
+        max-width: 300px;
+        width: 100%;
+        background: white;
+        box-shadow: 0px 0px 62px 0px rgba(0, 0, 0, 0.22);
+        border-radius: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .card__user-img {
+        border: 2px solid #2c5eff;
+        height: 70px;
+        width: 70px;
+        margin: 2rem 0 1.5rem;
+        border-radius: 50%;
+        object-fit: cover;
+        object-position: center;
+    }
+
+    .card__info {
+        margin-bottom: 0rem;
+    }
+
+    .card__name {
+        text-align: center;
+        display: block;
+        font-weight: 600;
+        font-size: 1.2rem;
+        margin-bottom: 0.5rem;
+    }
+
+    .card__title {
+        display: block;
+        font-size: 0.7rem;
+        margin-bottom: 2rem;
+        text-align: center;
+    }
+
+    .card__icon {
+        width: 200px;
+        padding: 10px;
+        border-radius: 3px;
+        border: 1px solid black;
+        margin-bottom: 0.4rem;
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: background 0.3s ease-in-out, color 0.3s ease-in-out;
+        cursor: pointer;
+    }
+
+    .card__icon-box {
+        width: 25px;
+        display: inline-block;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .card__icon-title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+    }
+
+    .card__icon--fb {
+        color: #2c5eff;
+        border: 1px solid #2c5eff;
+    }
+
+    .card__icon--fb:hover {
+        background: #2c5eff;
+        color: white;
+    }
+    .card__icon--ig {
+        color: #773fe7;
+        border: 1px solid #773fe7;
+    }
+
+    .card__icon--ig:hover {
+        background: #773fe7;
+        color: white;
+    }
+    .card__icon--db {
+        color: #f962b1;
+        border: 1px solid #f962b1;
+    }
+
+    .card__icon--db:hover {
+        background: #f962b1;
+        color: white;
+    }
+
+    .card__icon--gl {
+        color: #ff0000;
+        border: 1px solid #ff0000;
+    }
+
+    .card__icon--gl:hover {
+        background: #ff0000;
+        color: white;
+    }
+`;
+
+	body.prepend(root);
+	root.classList.add("root");
+	root.prepend(card);
+	root.style.cssText = `
+background: #197278;
+min-height: 100vh;
+`;
+	root.prepend(style);
+	card.classList.add("card");
+	style.innerHTML = cssStyles;
+	card.innerHTML = html;
+
+	let cardSocials = card.getElementsByClassName("card__socials")[0];
+
+	data.socials.forEach((item, index) => {
+		let tempNode = document.createElement("div");
+		tempNode.classList.add(`card__icon`, `card__icon--${item.id}`);
+		tempNode.innerHTML = `
+        <span class="card__icon-box">
+            <i class="${item.icon}"></i>
+        </span>
+        <span class="card__icon-title">
+            ${item.service}
+        </span>    
+    `;
+		cardSocials.append(tempNode);
+	});
+};
+
+cardApp(data);
