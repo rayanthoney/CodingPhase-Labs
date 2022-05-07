a6f4557b2b8c912a8c1abb60ac620b0e7e7683ed
Ray Anthoney <rmaxey911@gmail.com>
2022-05-06 18:57:08 -0600

    Codingphase practical JS

--- practical-javascipt/basics/app.js
+++ practical-javascipt/basics/app.js
@@ -1,38 +1,42 @@
-// console.log('Hi we just loaded the app.js')
-// console.log(2+2);
-// console.log('Ray ' + 'Anthoney')
+// console.log('Hi we just loaded the app.js') // STRING
+// console.log(2+2); // ADDITION
+// // console.log('Ray ' + 'Anthoney') // CONCATENATE (ADDING STRINGS TOGETHER)
 
 
 
 // function printName(first, last) {
-//     console.log(first + ' ' + last);
+//     console.log(first + ' ' + last); // CONCATENATE (ADDING STRINGS TOGETHER)
 //     alert(first + ' ' + last);
 // }
 
 
 // "Everything in programming is an an action that triggers a reaction"
 
-// Variables - is a bucket that you give data 
+// VARIABLES - IS A BUCKET THAT YOU GIVE DATA
     let username = "Ray Anthoney"
     console.log(username)
     username = "Mireya Elisse";
     console.log(username);
 
-// Constants
+// CONSTANT (CANOT BE REASSIGNED)
     const store = "Diamonds";
-    // username = "New Balance";
+    username = "New Balance";
     console.log(username);
 
-// Operators - symbol that tells the program an action 
+// Operators - symbol that tells the program an action
 
     // let x = 25;
     // x = 25 + 5;
     // console.log(x)
-   
+
+    // OR
+
     // let x = 25;
     // x = x + 5;
     // console.log(x)
-   
+
+    // OR
+
     let x = 25;
     x += 5;
     console.log(x)
@@ -45,67 +49,66 @@
     let y = 10;
     console.log(y)
 
-    let age = 18;
-    console.log(age == 21 && username == "Mireya Elisse");
+//     let age = 18;
+//     console.log(age == 21 && username == "Mireya Elisse");
 
-// Array - list of values 
-    let listAR = [23, 234, "billy", "joe", {title: 'harry potter'}];
+// // Array - list of values
+//     let listAR = [23, 234, "billy", "joe", {title: 'harry potter'}];
 
-    console.log(listAR)
+//     console.log(listAR)
 
-// Objects - A entity that holds values and methods
+// // Objects - A entity that holds values and methods
 
-    let user = {
-        name: "Kairos",
-        username: "MireyaElisse",
-        favoriteMovies: ['SW', 'Lord Of The Flies'],
-        age: 24,
-        princeCity: function () {
-            return 'New York City'
-        },
-        objectA: {
-            objectB: 245
-        }    
-    };
-    
-    console.log(user.objectA.objectB)
+//     let user = {
+//         name: "Kairos",
+//         username: "MireyaElisse",
+//         favoriteMovies: ['SW', 'Lord Of The Flies'],
+//         age: 24,
+//         princeCity: function () {
+//             return 'New York City'
+//         },
+//         objectA: {
+//             objectB: 245
+//         }
+//     };
+
+//     console.log(user.objectA.objectB)
 
 // Booleans - is it true or false
-    console.log(4 < 10);
+    // console.log(4 < 10);
 
 // Control Flow
-    if(age >=21){
-        console.log("User Can Drink!!")
-    } else {
-        console.log('Sorry your too young go home!!')
-    }
+    // if(age >=21){
+    //     console.log("User Can Drink!!")
+    // } else {
+    //     console.log('Sorry your too young go home!!')
+    // }
 
 // Expressions vs statements
-    console.log(20 > 30 ? "true" : "false")
+    // console.log(20 > 30 ? "true" : "false")
 
 // Functions - reusable code blocks
-    function printName(first, last) {
-        console.log(first + " " + last); // printName
-        return first + ' ' + last; // console.log
-    }
+    // function printName(first, last) {
+    //     console.log(first + " " + last); // printName
+    //     return first + ' ' + last; // console.log
+    // }
     // console.log( "Ray" + " " + "Anthoney")
     // console.log( "Mireya" + " " + "Elisse")
     // console.log( "Ashleigh" + " " + "Marie")
 
-    printName("Ray", "Anthoney");
-    printName("Mireya", "Elisse");
-    printName("Ashleigh", "Marie");
+    // printName("Ray", "Anthoney");
+    // printName("Mireya", "Elisse");
+    // printName("Ashleigh", "Marie");
 
 // Loops - are repeatable code blocks
-for(let i = 1; i < 11; i++){
-    printName("Ray", "Anthoney");
-    // console.log(i) // will run #'s 1 - 10
-}
+// for(let i = 1; i < 11; i++){
+//     printName("Ray", "Anthoney");
+//     // console.log(i) // will run #'s 1 - 10
+// }
 
 // Null, Infinity, NaN, Undefined,
-let car = null;
-console.log(car);
-console.log(1/0);
-console.log(24 * "Billy");
-    
-    
+// let car = null;
+// console.log(car);
+// console.log(1/0);
+// console.log(24 * "Billy");
+
