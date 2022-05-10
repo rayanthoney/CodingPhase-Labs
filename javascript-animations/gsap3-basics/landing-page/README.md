578243add5bdf48536a939003936262a55d5468e
Ray Anthoney <rmaxey911@gmail.com>
2022-05-10 09:51:45 -0600

    Codingphase creating barba transitions

---
+++ javascript-animations/gsap3-basics/landing-page/README.md
@@ -0,0 +1,49 @@
+# GSAP Landing Page
+
+# Codingphase Practical JavaScript project "Pokemon Game Lab"
+
+<br>
+<br>
+<br>
+
+<!-- Banner Section -->
+<h2 align="center">🛠️ Projects</h2>
+
+<br>
+<br>
+
+<div align="center" >
+    <a href=''>
+        <img src="#" alt="GSAP Landing Page" width="100%"/>
+    </a>
+
+<br>
+<br>
+
+<p>
+    <a href="https://github.com/rayanthoney/CodingPhase-Labs/tree/master/javascript-animations/gsap3-basics/landing-page" target="_blank">
+            <img src="https://img.shields.io/badge/Repo-lightgrey?style=plastic&zfor-the-badge&logo=github"/>
+    </a>
+    <a href="" target="_blank">
+        <img src="https://img.shields.io/badge/-website-red?style=plastic&zfor-the-badge&color=f92667"/>
+    </a>
+</p>
+</div>
+
+<br>
+
+## 🎨 This project was built using these technologies.
+
+<br>
+
+<p>
+<strong>JavaScript, CSS3, HTML5</strong> - JavaScript version of a Pokemon Game Lab created within the Codingphase "Future Proof JavaScript" course.
+</p>
+
+<br>
+
+[![HTML5 Badge](https://img.shields.io/badge/-HTML-E44D26?style=plastic&for-the-badge&labelColor=black&logo=html5&logoColor=E44D26)](#)&nbsp;
+[![CSS3 Badge](https://img.shields.io/badge/-CSS-1572B6?&style=plastic&for-the-badge&labelColor=black&logo=css3&logoColor=1572B6)](#)&nbsp;
+[![Javascript Badge](https://img.shields.io/badge/-Javascript-F0DB4F?style=plastic&for-the-badge&labelColor=black&logo=javascript&logoColor=F0DB4F)](#)&nbsp;
+[![GreenSock Badge](https://img.shields.io/badge/-GreenSock-88cf02?style=plastic&for-the-badge&labelColor=black&logo=greensock&logoColor=88cf02)](#)&nbsp;
+[![Barba.JS Badge](https://img.shields.io/badge/-Barba.JS-2e5bdc?style=plastic&for-the-badge&labelColor=black&logo=barba.js&logoColor=88cf02)](#)&nbsp;
--- javascript-animations/gsap3-basics/landing-page/js/app.js
+++ javascript-animations/gsap3-basics/landing-page/js/app.js
@@ -1,22 +1,9 @@
 const initialPageAnimation = () => {
-  let tl = gsap.timeline()
-tl.fromTo(
-  ".logo",
-  {
-    x: -200,
-    opacity: 0,
-  },
-  {
-    x: 0,
-    opacity: 1,
-    delay: 0.5,
-    duration: 1.3,
-  }
-)
-  .fromTo(
-    ".menu",
+    let tl = gsap.timeline()
+  tl.fromTo(
+    ".logo",
     {
-      x: 200,
+      x: -200,
       opacity: 0,
     },
     {
@@ -24,139 +11,152 @@ tl.fromTo(
       opacity: 1,
       delay: 0.5,
       duration: 1.3,
-    },
-    ">-1.8"
-  )
-  .fromTo(
-    ".info-section h1",
-    {
-      x: 0,
-      y: 100,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      y: 0,
-      opacity: 1,
-      duration: 1.3,
     }
   )
-  .addLabel("h1Show")
-  .fromTo(
-    ".girl",
-    {
-      height: 0,
-    },
-    {
-      height: 600,
-      duration: 1.4,
-      ease: "power2.inOut",
-    },
-    ">-.5"
-  )
-  .fromTo(
-    ".boy",
-    {
-      height: 0,
-    },
-    {
-      height: 600,
-      duration: 1.4,
-      ease: "power2.inOut",
-    },
-    ">-1"
-  )
-  .fromTo(
-    ".shape1",
-    {
-      scale: 0,
-      opacity: 0,
-    },
-    {
-      scale: 1,
-      opacity: 1,
-      ease: "power2.inOut",
-    },
-    ">-1"
-  )
-  .fromTo(
-    ".shape3",
-    {
-      opacity: 0,
-    },
-    {
-      opacity: 0.6,
-      ease: "power2.inOut",
-      duration: 1,
-    },
-    ">-.2"
-  )
-  .fromTo(
-    ".shape3",
-    {
-      x: -50,
-      y: 500,
-    },
-    {
-      y: 465,
-      repeat: -1,
-      duration: 1.5,
-      ease: "sine.inOut",
-      yoyo: true,
-    }
-  )
-  .fromTo(
-    ".shape2",
-    {
-      opacity: 0,
-    },
-    {
-      opacity: 0.3,
-      ease: "power2.inOut",
-      duration: 1,
-    },
-    ">-2"
-  )
-  .fromTo(
-    ".shape2",
-    {
-      x: 550,
-      y: -150,
-    },
-    {
-      y: -185,
-      repeat: -1,
-      duration: 1.5,
-      ease: "sine.inOut",
-      yoyo: true,
-    }
-  )
-  .fromTo(
-    ".info-section h4",
-    {
-      x: -50,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      opacity: 1,
-      duration: 1.3,
-    },
-    "h1Show"
-  )
-  .fromTo(
-    ".call-actions",
-    {
-      x: -50,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      opacity: 1,
-      duration: 1.3,
-    },
-    "h1Show+=1"
-  );
+    .fromTo(
+      ".menu",
+      {
+        x: 200,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        delay: 0.5,
+        duration: 1.3,
+      },
+      ">-1.8"
+    )
+    .fromTo(
+      ".info-section h1",
+      {
+        x: 0,
+        y: 100,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        duration: 1.3,
+      }
+    )
+    .addLabel("h1Show")
+    .fromTo(
+      ".girl",
+      {
+        height: 0,
+      },
+      {
+        height: 600,
+        duration: 1.4,
+        ease: "power2.inOut",
+      },
+      ">-.5"
+    )
+    .fromTo(
+      ".boy",
+      {
+        height: 0,
+      },
+      {
+        height: 600,
+        duration: 1.4,
+        ease: "power2.inOut",
+      },
+      ">-1"
+    )
+    .fromTo(
+      ".shape1",
+      {
+        scale: 0,
+        opacity: 0,
+      },
+      {
+        scale: 1,
+        opacity: 1,
+        ease: "power2.inOut",
+      },
+      ">-1"
+    )
+    .fromTo(
+      ".shape3",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 0.6,
+        ease: "power2.inOut",
+        duration: 1,
+      },
+      ">-.2"
+    )
+    .fromTo(
+      ".shape3",
+      {
+        x: -50,
+        y: 500,
+      },
+      {
+        y: 465,
+        repeat: -1,
+        duration: 1.5,
+        ease: "sine.inOut",
+        yoyo: true,
+      }
+    )
+    .fromTo(
+      ".shape2",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 0.3,
+        ease: "power2.inOut",
+        duration: 1,
+      },
+      ">-2"
+    )
+    .fromTo(
+      ".shape2",
+      {
+        x: 550,
+        y: -150,
+      },
+      {
+        y: -185,
+        repeat: -1,
+        duration: 1.5,
+        ease: "sine.inOut",
+        yoyo: true,
+      }
+    )
+    .fromTo(
+      ".info-section h4",
+      {
+        x: -50,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1.3,
+      },
+      "h1Show"
+    )
+    .fromTo(
+      ".call-actions",
+      {
+        x: -50,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1.3,
+      },
+      "h1Show+=1"
+    );
 }
 
 
@@ -167,6 +167,24 @@ const delay = (n) => {
     }, n)
   })
 }
+const loadingLeave= () => {
+  let timeline = gsap.timeline();
+  timeline.fromTo('.loading-bg', {
+    y: '100%',
+
+  },  {
+    y: 0,
+  })
+}
+const loadingEnter = () => {
+  let timeline = gsap.timeline();
+  timeline.fromTo('.loading-bg', {
+    y: 0
+  },  {
+    y: '100%',
+    duration: 2
+  })
+}
 barba.init({
   sync: true,
   transition: [
@@ -175,10 +193,12 @@ barba.init({
       async leave(data){
         const done = this.async();
         console.log('Leaving Page Animation');
+        loadingLeave();
         await delay(2000);
         done();
       },
       async enter(data){
+        loadingEnter();
         console.log('Entering Page Animation');
       }
     }
