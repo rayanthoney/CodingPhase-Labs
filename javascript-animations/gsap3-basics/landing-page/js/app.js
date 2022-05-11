c6f2de8288e24342e2cb069d5412ef749738ac69
Ray Anthoney <rmaxey911@gmail.com>
2022-05-11 06:49:38 -0600

    Codingphase creating gallery page

--- javascript-animations/gsap3-basics/landing-page/js/app.js
+++ javascript-animations/gsap3-basics/landing-page/js/app.js
@@ -1,206 +1,218 @@
 const initialPageAnimation = () => {
-    let tl = gsap.timeline()
-  tl.fromTo(
-    ".logo",
-    {
-      x: -200,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      opacity: 1,
-      delay: 0.5,
-      duration: 1.3,
-    }
-  )
-    .fromTo(
-      ".menu",
-      {
-        x: 200,
-        opacity: 0,
-      },
-      {
-        x: 0,
-        opacity: 1,
-        delay: 0.5,
-        duration: 1.3,
-      },
-      ">-1.8"
-    )
-    .fromTo(
-      ".info-section h1",
-      {
-        x: 0,
-        y: 100,
-        opacity: 0,
-      },
-      {
-        x: 0,
-        y: 0,
-        opacity: 1,
-        duration: 1.3,
-      }
-    )
-    .addLabel("h1Show")
-    .fromTo(
-      ".girl",
-      {
-        height: 0,
-      },
-      {
-        height: 600,
-        duration: 1.4,
-        ease: "power2.inOut",
-      },
-      ">-.5"
-    )
-    .fromTo(
-      ".boy",
-      {
-        height: 0,
-      },
-      {
-        height: 600,
-        duration: 1.4,
-        ease: "power2.inOut",
-      },
-      ">-1"
-    )
-    .fromTo(
-      ".shape1",
-      {
-        scale: 0,
-        opacity: 0,
-      },
-      {
-        scale: 1,
-        opacity: 1,
-        ease: "power2.inOut",
-      },
-      ">-1"
-    )
-    .fromTo(
-      ".shape3",
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 0.6,
-        ease: "power2.inOut",
-        duration: 1,
-      },
-      ">-.2"
-    )
-    .fromTo(
-      ".shape3",
-      {
-        x: -50,
-        y: 500,
-      },
-      {
-        y: 465,
-        repeat: -1,
-        duration: 1.5,
-        ease: "sine.inOut",
-        yoyo: true,
-      }
-    )
-    .fromTo(
-      ".shape2",
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 0.3,
-        ease: "power2.inOut",
-        duration: 1,
-      },
-      ">-2"
-    )
-    .fromTo(
-      ".shape2",
-      {
-        x: 550,
-        y: -150,
-      },
-      {
-        y: -185,
-        repeat: -1,
-        duration: 1.5,
-        ease: "sine.inOut",
-        yoyo: true,
-      }
-    )
-    .fromTo(
-      ".info-section h4",
-      {
-        x: -50,
-        opacity: 0,
-      },
-      {
-        x: 0,
-        opacity: 1,
-        duration: 1.3,
-      },
-      "h1Show"
-    )
-    .fromTo(
-      ".call-actions",
-      {
-        x: -50,
-        opacity: 0,
-      },
-      {
-        x: 0,
-        opacity: 1,
-        duration: 1.3,
-      },
-      "h1Show+=1"
-    );
-}
+	let tl = gsap.timeline();
+	tl.fromTo(
+		".logo",
+		{
+			x: -200,
+			opacity: 0,
+		},
+		{
+			x: 0,
+			opacity: 1,
+			delay: 0.5,
+			duration: 1.3,
+		}
+	)
+		.fromTo(
+			".menu",
+			{
+				x: 200,
+				opacity: 0,
+			},
+			{
+				x: 0,
+				opacity: 1,
+				delay: 0.5,
+				duration: 1.3,
+			},
+			">-1.8"
+		)
+		.fromTo(
+			".info-section h1",
+			{
+				x: 0,
+				y: 100,
+				opacity: 0,
+			},
+			{
+				x: 0,
+				y: 0,
+				opacity: 1,
+				duration: 1.3,
+			}
+		)
+		.addLabel("h1Show")
+		.fromTo(
+			".girl",
+			{
+				height: 0,
+			},
+			{
+				height: 600,
+				duration: 1.4,
+				ease: "power2.inOut",
+			},
+			">-.5"
+		)
+		.fromTo(
+			".boy",
+			{
+				height: 0,
+			},
+			{
+				height: 600,
+				duration: 1.4,
+				ease: "power2.inOut",
+			},
+			">-1"
+		)
+		.fromTo(
+			".shape1",
+			{
+				scale: 0,
+				opacity: 0,
+			},
+			{
+				scale: 1,
+				opacity: 1,
+				ease: "power2.inOut",
+			},
+			">-1"
+		)
+		.fromTo(
+			".shape3",
+			{
+				opacity: 0,
+			},
+			{
+				opacity: 0.6,
+				ease: "power2.inOut",
+				duration: 1,
+			},
+			">-.2"
+		)
+		.fromTo(
+			".shape3",
+			{
+				x: -50,
+				y: 500,
+			},
+			{
+				y: 465,
+				repeat: -1,
+				duration: 1.5,
+				ease: "sine.inOut",
+				yoyo: true,
+			}
+		)
+		.fromTo(
+			".shape2",
+			{
+				opacity: 0,
+			},
+			{
+				opacity: 0.3,
+				ease: "power2.inOut",
+				duration: 1,
+			},
+			">-2"
+		)
+		.fromTo(
+			".shape2",
+			{
+				x: 550,
+				y: -150,
+			},
+			{
+				y: -185,
+				repeat: -1,
+				duration: 1.5,
+				ease: "sine.inOut",
+				yoyo: true,
+			}
+		)
+		.fromTo(
+			".info-section h4",
+			{
+				x: -50,
+				opacity: 0,
+			},
+			{
+				x: 0,
+				opacity: 1,
+				duration: 1.3,
+			},
+			"h1Show"
+		)
+		.fromTo(
+			".call-actions",
+			{
+				x: -50,
+				opacity: 0,
+			},
+			{
+				x: 0,
+				opacity: 1,
+				duration: 1.3,
+			},
+			"h1Show+=1"
+		);
+};
 
+// BARBAS.JS SECTION
 
 const delay = (n) => {
-  return new Promise((done) => {
-    setTimeout(() => {
-      done();
-    }, n)
-  })
-}
-const loadingLeave= () => {
-  let timeline = gsap.timeline();
-  timeline.fromTo('.loading-bg', {
-    y: '100%',
-
-  },  {
-    y: 0,
-  })
-}
+	return new Promise((done) => {
+		setTimeout(() => {
+			done();
+		}, n);
+	});
+};
+const loadingLeave = () => {
+	let timeline = gsap.timeline();
+	timeline.fromTo(
+		".loading-bg",
+		{
+			y: "100%",
+		},
+		{
+			y: 0,
+		}
+	);
+};
 const loadingEnter = () => {
-  let timeline = gsap.timeline();
-  timeline.fromTo('.loading-bg', {
-    y: 0
-  },  {
-    y: '100%',
-    duration: 2
-  })
-}
+	let timeline = gsap.timeline();
+	timeline.fromTo(
+		".loading-bg",
+		{
+			y: 0,
+		},
+		{
+			y: "100%",
+			duration: 2,
+		}
+	);
+};
 barba.init({
-  sync: true,
-  transition: [
-    {
-      name: 'page-wipe',
-      async leave(data){
-        const done = this.async();
-        console.log('Leaving Page Animation');
-        loadingLeave();
-        await delay(2000);
-        done();
-      },
-      async enter(data){
-        loadingEnter();
-        console.log('Entering Page Animation');
-      }
-    }
-  ]
-})
+	sync: true,
+	transitions: [
+		{
+			name: "page-wipe",
+			async leave(data) {
+				const done = this.async();
+				console.log("Leaving Page Animation");
+				loadingLeave();
+				await delay(1500);
+				done();
+			},
+			async enter(data) {
+				loadingEnter();
+				initialPageAnimation();
+				console.log("Entering Page Animation");
+			},
+			async once(data) {
+				initialPageAnimation();
+			},
+		},
+	],
+});
