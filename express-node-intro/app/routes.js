0a64408c5d3b4cf7761f932c0e4a568eaa7d7343
Ray Anthoney <rmaxey911@gmail.com>
2022-08-03 17:22:35 -0600

    added a post file to project

--- express-node-intro/app/routes.js
+++ express-node-intro/app/routes.js
@@ -10,6 +10,10 @@ router.get("/generic", (req, res) => {
   return res.render("../assets/views/generic.pug");
 });
 
+router.get("/post/create", (req, res) => {
+  return res.render("../assets/views/post/create.pug");
+});
+
 router.get("/pug", (req, res) => {
   return res.render("../assets/views/testing.pug", {
     username: "codingphase",
